86af623b86de3f86ad7059ac325667fd71f26aee
Bayode Gabriel <bayogabriel24@gmail.com>
2023-01-03 12:00:00 +0100

    V1.0

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+import im from "@/public/footer.svg"
 export default function Footer() {
   return (
-    <footer className="footer min-h-[173px]">
-      
+    <footer className="min-h-[173px]">
+      <Image src={im} alt="gfod" className="w-full h-full"/>
     </footer>
   )
 }
