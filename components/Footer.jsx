5bf02bb09f111409fca0e0dcd5877856ada46f50
Bayode Gabriel <bayogabriel24@gmail.com>
2023-01-07 12:00:00 +0100

    V1.0

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,22 @@
 import Image from "next/image";
 import im from "@/public/footer.svg"
+import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="min-h-[173px] footer w-full flex items-center justify-between">
-      <div className="">
-        
+      <div className="flex items-center gap-6">
+        <span className="text-white text-[20px]">
+        © 2025  Lokraft.com Ltd. All rights reserved.
+        </span>
+        <div className="flex items-center gap-3 text-[20px]">
+          <Link href="#" className="text-white">Terms of Service</Link>
+          <Link href="#" className="text-white">Privacy Policy</Link>
+        </div>  
+      </div>
+      <div className="flex items-center gap-4">
+        <Link href="" className="rounded-full border-white text-white p-2 flex items-center justify-center">
+          <FaXTwitter />
+        </Link>
       </div>
     </footer>
   )
