020d5094c3146fd6d4c31f15c2413be5a1ea9c28
Bayode Gabriel <bayogabriel24@gmail.com>
2023-01-08 12:00:00 +0100

    V1.0

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import im from "@/public/footer.svg"
+import { FaXTwitter } from "react-icons/fa6";
 import Link from "next/link";
 export default function Footer() {
   return (
-    <footer className="min-h-[173px] footer w-full flex items-center justify-between">
+    <footer className="min-h-[173px] bg-[#012D26] w-full flex items-center justify-between px-[100px]">
       <div className="flex items-center gap-6">
         <span className="text-white text-[20px]">
         © 2025  Lokraft.com Ltd. All rights reserved.
@@ -14,7 +14,7 @@ export default function Footer() {
         </div>  
       </div>
       <div className="flex items-center gap-4">
-        <Link href="" className="rounded-full border-white text-white p-2 flex items-center justify-center">
+        <Link href="" className="rounded-full border-white text-white border p-2 flex items-center justify-center">
           <FaXTwitter />
         </Link>
       </div>
