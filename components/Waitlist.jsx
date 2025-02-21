09a150f77f42d6660c667307b001ab227e34b62f
Bayode Gabriel <bayogabriel24@gmail.com>
2025-02-21 18:11:24 +0000

    Feb 21, 2025, 7:11 PM

--- components/Waitlist.jsx
+++ components/Waitlist.jsx
@@ -5,8 +5,12 @@ const Waitlist = () => {
   return (
     <div className='w-full flex items-center justify-center p-[80px] relative' id='waitlist'>
         <Image src={svg} alt='svg' className='absolute left-0 -z-0 -top-2'/>
-      <div className="w-full wait rounded-[30px] p-[100px] flex items-center z-[2]">
+      <div className="w-full wait rounded-[30px] px-[100px] py-20 flex items-center z-[2] gap-[50px]">
         <h3 className="font-[800] text-[40px] text-white">Enjoy a <span className="text-[#94D227]">5%</span> discount on your first 3 orders when we launch.</h3>
+        <div className="relative">
+          <input type="email" name='email' className='w-[610px] py-4 px-8 placeholder:text-white text-white bg-[#5E5D5C] outline-none' placeholder='Your email' />
+          <button className="text-white bg-[#F9A607]">Join the waitlist</button>
+        </div>
       </div>
     </div>
   )
